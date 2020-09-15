1bd63d0e20137a66bccf709d888f2fc397a2b07c
黄(Huáng)瓒(Zàn) <13297503+pmixer@users.noreply.github.com>
2020-09-15 16:27:55 -0400

    fix a small bug for mask generation

--- modules.py
+++ modules.py
@@ -192,7 +192,7 @@ def multihead_attention(queries,
         outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
         
         # Key Masking
-        key_masks = tf.sign(tf.abs(tf.reduce_sum(keys, axis=-1))) # (N, T_k)
+        key_masks = tf.sign(tf.reduce_sum(tf.abs(keys, axis=-1))) # (N, T_k)
         key_masks = tf.tile(key_masks, [num_heads, 1]) # (h*N, T_k)
         key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(queries)[1], 1]) # (h*N, T_q, T_k)
         
@@ -211,7 +211,7 @@ def multihead_attention(queries,
         # Activation
         outputs = tf.nn.softmax(outputs)
         # Query Masking
-        query_masks = tf.sign(tf.abs(tf.reduce_sum(queries, axis=-1))) # (N, T_q)
+        query_masks = tf.sign(tf.reduce_sum(tf.abs(queries, axis=-1))) # (N, T_q)
         query_masks = tf.tile(query_masks, [num_heads, 1]) # (h*N, T_q)
         query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, tf.shape(keys)[1]]) # (h*N, T_q, T_k)
         outputs *= query_masks # broadcasting. (N, T_q, C)
